960d77965a24a68bc256012c7050c8644bbf3a9f
tsrigo <717660037@qq.com>
2022-10-11 20:30:19 +0800

    hw02: q1: simplify calling contructor function

--- hw02/q1/test_Rectangle.cpp
+++ hw02/q1/test_Rectangle.cpp
@@ -3,8 +3,8 @@
 #include"Rectangle.h"
 
 int main() {
-    Rectangle R1 = Rectangle(4, 40);
-    Rectangle R2 = Rectangle(3.5, 35.9);
+    Rectangle R1(4, 40);
+    Rectangle R2(3.5, 35.9);
     printf("Area of R1 is %f. Perimeter of R1 is %f\n", R1.getArea(), R1.getPerimeter());
     printf("Area of R2 is %f. Perimeter of R2 is %f\n", R2.getArea(), R2.getPerimeter());
     return 0;
